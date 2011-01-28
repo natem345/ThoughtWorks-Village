18e6cd1747b0278600bd7f1cc238625bcca3154c
__ <_@__.com>
2011-01-27 19:02:27 -1000

    surveys

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -14,8 +14,7 @@ class SurveysController < ApplicationController
   # GET /surveys/1.xml
   def show
     @survey = Survey.find(params[:id])
-    @response = Response.new
-
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @survey }
